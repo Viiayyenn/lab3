2d587223efc5a18136d749e569509a390ca43102
Viiayyenn <119033162+Viiayyenn@users.noreply.github.com>
2025-06-03 17:06:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, World!"
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=10000)
+    # Chạy ở chế độ debug để dễ phát triển
+    app.run(host='0.0.0.0', port=5000, debug=True)
